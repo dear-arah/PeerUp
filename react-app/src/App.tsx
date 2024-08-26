12fdd8eb6c540e5dcde79b9dfb26f189ae1929fd
dear-arah <dearahmbarsolasco@gmail.com>
2024-08-26 15:54:55 +0800

    First Commit

---
+++ react-app/src/App.tsx
@@ -0,0 +1,10 @@
+import LandingPage from "./components/LandingPage.tsx";
+import "./App.css";
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet"></link>
+
+function App(){
+  return <div> <LandingPage /> </div>;
+}
+
+
+export default App;
---
+++ react-app/src/components/LandingPage.tsx
@@ -0,0 +1,41 @@
+function LandingPage() {
+    return (
+      <div className="LandingPage">
+        <div className="LandingPageLeft">
+          <h1 className="PeerUpLandingPage">PeerUp</h1>
+          <p className="Tagline">Upskill with a mentor perfectly paired for you.</p>
+  
+          <div className="auth-tabs">
+            <p className="active-tab">Login</p>
+            <p className="inactive-tab">Sign Up</p>
+          </div>
+  
+          <div className="input-fields">
+            <div className="input-wrapper">
+              <label>Username</label>
+              <input type="text" placeholder="Enter Username" />
+            </div>
+            <div className="input-wrapper">
+              <label>Password</label>
+              <input type="password" placeholder="Enter Password" />
+            </div>
+          </div>
+  
+          <button className="login-button">Login</button>
+          <p className="forgot-password">Forgot Password?</p>
+        </div>
+  
+        <div className="LandingPageRight">
+          <div className="circle1"></div>
+          <div className="circle2"></div>
+          <div className="circle3"></div>
+          <div className="circle4"></div>
+          <img src="./resources/arrow.svg" alt="Arrow" className="arrow" />
+        </div>
+      </div>
+    );
+  }
+  
+
+
+export default LandingPage;
